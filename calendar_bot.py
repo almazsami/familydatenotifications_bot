29b706a59106fee04bf2a33f59e9c791b689a11c
almazsami <samigullin.almaz.r@gmail.com>
2026-02-13 10:59:43 +0300

    Create calendar_bot.py

---
+++ calendar_bot.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime
+
+BOT_TOKEN = "TOKEN_WILL_BE_FROM_GITHUB_SECRET"
+
+CHAT_ID = -1003809412436
+THREAD_ID = 5
+
+BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
+def get_updates():
+    return requests.get(f"{BASE_URL}/getUpdates").json()
+
+def reply(message_id):
+    requests.post(
+        f"{BASE_URL}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "message_thread_id": THREAD_ID,
+            "reply_to_message_id": message_id,
+            "text": "@almazsami @dinara.qwq"
+        }
+    )
+
+today = datetime.utcnow().strftime("%d.%m.%Y")
+
+updates = get_updates()
+
+for u in updates.get("result", []):
+    msg = u.get("message")
+    if not msg:
+        continue
+
+    if msg.get("message_thread_id") != THREAD_ID:
+        continue
+
+    text = msg.get("text", "")
+    if text.startswith(today):
+        reply(msg["message_id"])
